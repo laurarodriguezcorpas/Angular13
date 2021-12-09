5a9197472239ad7c262d5abe9b9a30e4c06b8dd7
laura <lrcorpas96@hotmail.com>
2021-12-09 13:16:24 +0100

    Basic counter finished

--- Angular13/src/app/app.component.ts
+++ Angular13/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular13';
+  title = 'COUNTER APP';
+  number: number = 10;
+
+  accumulate( value: number ) {
+    this.number += value;
+  }
 }
