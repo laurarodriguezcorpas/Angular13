be21eb539b2d4385e659a4639d2b167d5dca397a
laura <lrcorpas96@hotmail.com>
2021-12-14 17:44:07 +0100

    "HeroesModule" module added

--- Angular13/src/app/app.module.ts
+++ Angular13/src/app/app.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CounterModule],
+=======
+import { HeroesModule } from './heroes/heroes.module';
+import { HeroesComponent } from './heroes/heroes/heroes.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeroesModule],
+>>>>>>> 4ebd7f2 ("HeroesModule" module added)
   providers: [],
   bootstrap: [AppComponent],
 })
