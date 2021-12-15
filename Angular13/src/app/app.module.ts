e2208088005da8afc5584be8b1b1a1d0f9897b80
laura <lrcorpas96@hotmail.com>
2021-12-15 14:33:04 +0100

    COUNTER AND HEROES MODULES COMPLETELY RESTORED :)

--- Angular13/src/app/app.module.ts
+++ Angular13/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [AppComponent],
-  imports: [BrowserModule, HeroesModule],
-=======
-  declarations: [AppComponent, HeroesModule],
-  imports: [BrowserModule],
->>>>>>> 8b08b183325d2a900751afb2388dcef859baf95d
+  imports: [BrowserModule, HeroesModule, CounterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
