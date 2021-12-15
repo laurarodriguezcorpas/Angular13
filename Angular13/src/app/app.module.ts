97d6be045080597a4427a5587b119bc6ca9504a5
laura <lrcorpas96@hotmail.com>
2021-12-15 14:22:25 +0100

    dummy3

--- Angular13/src/app/app.module.ts
+++ Angular13/src/app/app.module.ts
@@ -5,8 +5,13 @@ import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
+<<<<<<< HEAD
   declarations: [AppComponent],
   imports: [BrowserModule, HeroesModule],
+=======
+  declarations: [AppComponent, HeroesModule],
+  imports: [BrowserModule],
+>>>>>>> 8b08b183325d2a900751afb2388dcef859baf95d
   providers: [],
   bootstrap: [AppComponent],
 })
